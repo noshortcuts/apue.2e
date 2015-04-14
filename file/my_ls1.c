e650c7f525d520e647ec5485a9526adcf133658f
yc1992 <jiejin264@qq.com>
2015-04-15 00:49:52 +0800

    first version

---
+++ file/my_ls1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <dirent.h>
+int main(int argc, char *argv[])
+{
+	DIR 	*dp;
+	struct 	dirent 	*dirp;
+	if(argc != 2)
+		printf("usage:ls directory_name\n");
+	if ((dp = opendir(argv[1])) == NULL)
+	{
+		printf("can't open %s\n", argv[1]);	
+	}
+	while ((dirp = readdir(dp)) != NULL)
+		printf("%s\n",dirp->d_name);
+	closedir(dp);
+	return 0;
+}
